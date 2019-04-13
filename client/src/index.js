46ce718e2502b503730c74c7ad2c9d880e30786a
Mikael Skoglund <mikael.skoglund@daresay.co>
2019-04-13 13:14:54 +0200

    it works

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,75 @@
 import "./styles/main.scss";
 
-import io from "socket.io";
+import io from "socket.io-client";
 
-const socket = io();
-const canvas = document.querySelector("#surface");
+window.onclick = () => {
+  const audioCtx = new AudioContext();
+  const bufferSource = audioCtx.createBufferSource();
+  const filterNode = audioCtx.createBiquadFilter();
+  const delayNode = audioCtx.createDelay(5.0);
+  const delayGainNode = audioCtx.createGain();
+  delayNode.delayTime.value = 148 / 60 / 2;
 
-canvas.addEventListener("mousemove", e => {
-  const y = e.clientY / canvas.clientHeight;
-  const x = e.clientX / canvas.clientWidth;
+  bufferSource.connect(filterNode);
 
-  console.log(y);
-  console.log(x);
-});
+  filterNode.connect(audioCtx.destination);
+  filterNode.connect(delayGainNode);
+  delayGainNode.connect(delayNode);
+  filterNode.frequency.value = audioCtx.sampleRate / 2;
+
+  delayNode.connect(audioCtx.destination);
+  console.log(delayNode);
+
+  const fileLoc = "/hackstreet-2.mp3";
+  fetch(fileLoc)
+    .then(response => response.arrayBuffer())
+    .then(buffer =>
+      audioCtx.decodeAudioData(buffer, data => {
+        bufferSource.buffer = data;
+        bufferSource.start();
+      })
+    );
+
+  const socket = io("http://localhost:8000", {
+    query: { djId: sessionStorage.getItem("djId") }
+  });
+
+  socket.on("connect", () => {});
+
+  const state = {
+    dj: null
+  };
+
+  const setState = newState => {
+    Object.assign(state, newState);
+    console.log(newState);
+  };
+
+  socket.on("dj_assigned", data => {
+    sessionStorage.setItem("djId", data.djId);
+  });
+
+  socket.on("coordinate_update", data => {
+    const frequency = Math.floor(
+      (data.filterDelay.x * audioCtx.sampleRate) / 2
+    );
+
+    const delay = data.filterDelay.y;
+
+    filterNode.frequency.setValueAtTime(frequency, audioCtx.currentTime);
+    delayGainNode.gain.setValueAtTime(delay, audioCtx.currentTime);
+  });
+
+  const canvas = document.querySelector("#surface");
+
+  canvas.addEventListener("mousemove", e => {
+    const y = e.clientY / canvas.clientHeight;
+    const x = e.clientX / canvas.clientWidth;
+
+    socket.emit("coordinate_update", {
+      djId: sessionStorage.getItem("djId"),
+      x,
+      y
+    });
+  });
+};
---
+++ server/index.js
@@ -0,0 +1,92 @@
+// Setup basic express server
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const port = process.env.PORT || 8000;
+
+server.listen(port, function() {
+  console.log("Server listening at port %d", port);
+});
+
+// Routing
+app.use(express.static("public"));
+
+// room
+let numUsers = 0;
+
+// filter state
+const effectsState = {
+  filterDelay: {
+    x: null,
+    y: null
+  },
+  reverb: {
+    x: null,
+    y: null
+  }
+};
+
+const djs = {
+  nick: null,
+  kingen: null
+};
+
+const djToEffect = {
+  nick: "filterDelay",
+  kingen: "reverb"
+};
+
+const findDjById = id => Object.keys(djs).find(key => djs[key] === id);
+
+io.on("connection", function(socket) {
+  // request comes in
+  // if we know client
+  // then dont increment this thing
+  ++numUsers;
+
+  let djId = socket.handshake.query.djId;
+  let currentDj = findDjById(socket.handshake.query.djId);
+
+  if (!currentDj) {
+    const availableSlots = Object.keys(djs).filter(key => {
+      return !djs[key];
+    });
+
+    currentDj = availableSlots[0];
+    djs[currentDj] = socket.id;
+    djId = socket.id;
+  }
+
+  socket.emit("dj_assigned", {
+    name: currentDj,
+    djId
+  });
+
+  // echo globally (all clients) that a person has connected
+  socket.broadcast.emit("user joined", {
+    numUsers: numUsers
+  });
+
+  // when the client emits 'new message', this listens and executes
+  socket.on("coordinate_update", function(data) {
+    const currentDj = findDjById(data.djId);
+    const effectName = djToEffect[currentDj];
+    effectsState[effectName] = {
+      x: data.x,
+      y: data.y
+    };
+    io.emit("coordinate_update", effectsState);
+  });
+
+  // when the user disconnects.. perform this
+  socket.on("disconnect", function() {
+    --numUsers;
+
+    // echo globally that this client has left
+    socket.broadcast.emit("user left", {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+});
